ab6bc6a07d1edb79efc75fa90925f4d7acea6061
tithichoudhary <79194811+tithichoudhary@users.noreply.github.com>
2021-06-26 10:31:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-print "hi from TF!"
+print "Hi from TF!"
+print "My Skills:"
+  print "➢ Programming Languages : C, C++, Python, Core Java (Beginner).
+         ➢ Web Technologies : HTML, CSS, PHP.
+         ➢ Databases : MySQL, MongoDB.
+         ➢ Other Skills : Machine Learning, Data Science, AutoCAD,
+                           Wire Framing"
 ?>
